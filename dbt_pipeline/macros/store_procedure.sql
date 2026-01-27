a4099f27ac0e670bbc6f92e21803da03c25f5626
Sandyprof <vsanthosh2003@gmail.com>
2026-01-27 18:27:46 +0530

    v2

--- dbt_pipeline/macros/store_procedure.sql
+++ dbt_pipeline/macros/store_procedure.sql
@@ -1,7 +1,7 @@
-{% macro run_log_sp() %}
-    {% set sql %}
-        CALL dbt_test1.default.log_message('Called using dbt run-operation');
-    {% endset %}
+--{% macro run_log_sp() %}
+   -- {% set sql %}
+   --     CALL dbt_test1.default.log_message('Called using dbt run-operation');
+   -- {% endset %}
 
-    {% do run_query(sql) %}
-{% endmacro %}
+  --  {% do run_query(sql) %}
+--{% endmacro %}
